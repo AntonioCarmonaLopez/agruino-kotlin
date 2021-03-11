4b0aed0103af6f030ae0aca33785aa1c1ecec32a
Antonio Carmona López <antocarmona@gmail.com>
2021-03-11 17:56:25 +0100

    Update sendMsgTelerang.sh

--- arduino/sendMsgTelerang.sh
+++ arduino/sendMsgTelerang.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-TOKEN="938242147:AAFo6bm2ggr6QNVETIGFr8hYPlGho29N13I"
-ID="519481083"
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+your credentials here
 
 curl -s -X POST $URL -d chat_id=$ID -d text=$1
